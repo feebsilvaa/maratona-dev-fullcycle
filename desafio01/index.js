e3addd7036792f76cfc0b8225f9df19c4d083ed0
Fernando Barbosa <fernando.barbosa@meta.com.br>
2020-05-04 18:39:41 -0300

    desafio 01 - criando webserver

---
+++ desafio01/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  let message = {
+    status: 'Hello World!'
+  }
+  res.json(message);
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at localhost: ${port}`)
+  console.log('Maratona Full Cycle 2.0');
+});
